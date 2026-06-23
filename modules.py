537c853fd0727f77a46491bf4c7963995e24721b
Shubh930 <shubhendrarajawat1@gmail.com>
2026-06-23 21:52:07 +0530

    added this having all libraries and packages

---
+++ modules.py
@@ -0,0 +1,21 @@
+import json
+
+import uuid
+
+from datetime import datetime
+
+from getpass import getpass
+
+# maskpass and pwinput both are used same.btoh prints star
+import maskpass
+import pwinput
+
+
+food_store = r"C:\Users\shubhendra singh\OneDrive\Desktop\python\SHUBHrestro\Database\menu.json"
+
+item_store = r"C:\Users\shubhendra singh\OneDrive\Desktop\python\SHUBHrestro\Database\cart.json"
+
+orders_store = r"C:\Users\shubhendra singh\OneDrive\Desktop\python\SHUBHrestro\Database\orders.json"
+
+tables_store = r"C:\Users\shubhendra singh\OneDrive\Desktop\python\SHUBHrestro\Database\tables.json"
+
